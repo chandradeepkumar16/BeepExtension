6361a510cd581d90519ecb99901405a9391e0504
Chandradeep Kumar <chandradeepkumar2002.ck@gmail.com>
2023-07-28 00:25:32 +0530

    added realtime running timer

--- popup.js
+++ popup.js
@@ -8,12 +8,43 @@ document.addEventListener("DOMContentLoaded", function () {
     const timeInSeconds = parseInt(document.getElementById("time").value, 10);
     if (!isNaN(timeInSeconds) && timeInSeconds > 0) {
       chrome.runtime.sendMessage({ message: "start", time: timeInSeconds });
-      // startCountdown(timeInSeconds);
+      startCountdown(timeInSeconds);
     }
   });
 
   stopTimerButton.addEventListener("click", function () {
     chrome.runtime.sendMessage({ message: "stop" });
-    // stopCountdown();
+    stopCountdown();
   });
+
+  function startCountdown(timeInSeconds) {
+    let timer = timeInSeconds;
+    updateTimerDisplay(timerContainer, timer);
+
+    countdown = setInterval(function () {
+      // Assign the interval to the global countdown variable
+      timer--;
+      updateTimerDisplay(timerContainer, timer);
+
+      if (timer === 0) {
+        clearInterval(countdown);
+      }
+    }, 1000);
+  }
+
+  function stopCountdown() {
+    timerContainer.textContent = "00:00";
+    clearInterval(countdown); // Clear the interval when stopping the countdown
+  }
+
+  function updateTimerDisplay(container, timeInSeconds) {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    const formattedTime = `${padZero(minutes)}:${padZero(seconds)}`;
+    container.textContent = formattedTime;
+  }
+
+  function padZero(num) {
+    return num.toString().padStart(2, "0");
+  }
 });
